248710b0fef54ff5d47fc6940470bcdc3b694f66
leonliusha <sha.liu01@sap.com>
2017-11-17 20:32:15 +0800

    feature: add controller and type handler.

--- services/yansu-microservice-user/src/main/java/com/yansu/yansumicroserviceuser/enums/UserSexEnum.java
+++ services/yansu-microservice-user/src/main/java/com/yansu/yansumicroserviceuser/enums/UserSexEnum.java
@@ -1,6 +1,25 @@
 package com.yansu.yansumicroserviceuser.enums;
 
 public enum UserSexEnum {
-	MAN,
-	WOMAN
+	MAN(1),
+	WOMAN(0);
+	
+	private int value;
+	
+	private UserSexEnum(int value){
+		this.value = value;
+	}
+	
+	public static UserSexEnum fromValue(int value){
+		for(UserSexEnum sexEnum : UserSexEnum.values()){
+			if(sexEnum.value == value){
+				return sexEnum;
+			}
+		}
+		throw new IllegalArgumentException("Can not create sex from value "+value);
+	}
+	
+	public int toValue(){
+		return value;
+	}
 }
---
+++ services/yansu-microservice-user/src/main/java/com/yansu/yansumicroserviceuser/web/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.yansu.yansumicroserviceuser.web.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yansu.yansumicroserviceuser.domain.User;
+import com.yansu.yansumicroserviceuser.mapper.UserMapper;
+
+@RestController
+public class UserController {
+	@Autowired
+	private UserMapper userMapper;
+	
+	@GetMapping("/getUsers")
+	public List<User> getUsers(){
+		List<User> users = userMapper.getAll();
+		return users;
+	}
+	
+	@GetMapping("/{id}")
+	public User findById(@PathVariable Long id){
+		User user = userMapper.getOne(id);
+		return user;
+	} 
+}
