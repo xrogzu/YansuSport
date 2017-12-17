29eea06c2de1771bbf4b10c667af6533cb771c29
leonliusha <leonliusha@gmail.com>
2017-12-17 23:33:43 +0800

    Feature: add CMS to service.

--- services/yansu-microservice-cms/site/src/main/java/org/yansu/yansusport/beans/BaseDocument.java
+++
@@ -1,9 +0,0 @@
-package org.yansu.yansusport.beans;
-
-import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoDocument;
-
-@Node(jcrType="yansusport:basedocument")
-public class BaseDocument extends HippoDocument {
-
-}
